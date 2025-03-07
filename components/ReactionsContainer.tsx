8f1817944a63bc6ec1ef7940d37aaa5c5a957ea5
raahimkhan <muhammadr915@gmail.com>
2025-03-07 21:14:58 +0500

    added animations to the gifs container

--- components/ReactionsContainer.tsx
+++ components/ReactionsContainer.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     StyleSheet,
-    View,
+    Animated,
 } from 'react-native';
 import {
     widthPercentageToDP as wp,
@@ -20,25 +20,56 @@ const ReactionsContainer: React.FC<ReactionsContainerProps> = ({
     screenSpacePercentage,
 }) => {
 
+    const opacityAnim = React.useRef(new Animated.Value(0)).current;
+    const translateY = React.useRef(new Animated.Value(reactionButtonPosition.y)).current;
+    const translateX = React.useRef(new Animated.Value(reactionButtonPosition.x)).current;
+
+    useEffect(() => {
+        Animated.parallel([
+            Animated.timing(opacityAnim, {
+                toValue: 1,
+                duration: 500,
+                useNativeDriver: true,
+            }),
+            Animated.timing(translateY, {
+                toValue: screenSpacePercentage.above > 20 ? reactionButtonPosition.y - hp(8) : reactionButtonPosition.y + hp(8),
+                duration: 200,
+                useNativeDriver: true,
+            }),
+            Animated.timing(translateX, {
+                toValue: screenSpacePercentage.left < 35 ? reactionButtonPosition.x + wp(8) : reactionButtonPosition.x - wp(16),
+                duration: 200,
+                useNativeDriver: true,
+            }),
+        ]).start();
+    }, []);
+
     return (
-        <View style={[styles.reactionsContainer, {
-            top: screenSpacePercentage.above > 20 ? reactionButtonPosition.y - hp(8) : reactionButtonPosition.y + hp(8),
-            left: screenSpacePercentage.left < 35 ? reactionButtonPosition.x + wp(8) : reactionButtonPosition.x - wp(16),
-        }]}>
+        <Animated.View style={[
+            styles.reactionsContainer, {
+                opacity: opacityAnim,
+                transform: [
+                    { translateY },
+                    { translateX },
+                ],
+            }
+        ]}>
             {ReactionData.map((reaction) => (
-                reaction.reactionID !== 1 && reaction.reactionID !== 7 && <Image
-                    key={reaction.reactionID}
-                    style={{
-                        flex: 1,
-                        width: wp(10),
-                        height: wp(10),
-                    }}
-                    source={reaction.reactionGif}
-                    contentFit="contain"
-                    contentPosition="left"
-                />
+                reaction.reactionID !== 1 && reaction.reactionID !== 7 && (
+                    <Image
+                        key={reaction.reactionID}
+                        style={{
+                            flex: 1,
+                            width: wp(10),
+                            height: wp(10),
+                        }}
+                        source={reaction.reactionGif}
+                        contentFit="contain"
+                        contentPosition="left"
+                    />
+                )
             ))}
-        </View>
+        </Animated.View>
     );
 };
 
