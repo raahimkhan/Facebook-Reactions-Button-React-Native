6380b204c4acd757a4b73db080e587bacf39ea3f
raahimkhan <muhammadr915@gmail.com>
2025-03-07 20:42:20 +0500

    added animated gifs inside the gif container

--- components/ReactionsContainer.tsx
+++ components/ReactionsContainer.tsx
@@ -7,6 +7,8 @@ import {
     widthPercentageToDP as wp,
     heightPercentageToDP as hp 
 } from 'react-native-responsive-screen';
+import { Image } from 'expo-image';
+import { ReactionData } from '@utilities/reactionData';
 
 interface ReactionsContainerProps {
     reactionButtonPosition: { x: number; y: number };
@@ -17,11 +19,25 @@ const ReactionsContainer: React.FC<ReactionsContainerProps> = ({
     reactionButtonPosition,
     screenSpacePercentage,
 }) => {
+
     return (
         <View style={[styles.reactionsContainer, {
             top: screenSpacePercentage.above > 20 ? reactionButtonPosition.y - hp(8) : reactionButtonPosition.y + hp(8),
             left: screenSpacePercentage.left < 35 ? reactionButtonPosition.x + wp(8) : reactionButtonPosition.x - wp(16),
         }]}>
+            {ReactionData.map((reaction) => (
+                reaction.reactionID !== 1 && reaction.reactionID !== 7 && <Image
+                    key={reaction.reactionID}
+                    style={{
+                        flex: 1,
+                        width: wp(10),
+                        height: wp(10),
+                    }}
+                    source={reaction.reactionGif}
+                    contentFit="contain"
+                    contentPosition="left"
+                />
+            ))}
         </View>
     );
 };
@@ -29,10 +45,13 @@ const ReactionsContainer: React.FC<ReactionsContainerProps> = ({
 const styles = StyleSheet.create({
     reactionsContainer: {
         position: 'absolute',
-        width: wp(50),
-        height: hp(6),
-        backgroundColor: 'red',
+        width: wp(53),
+        backgroundColor: '#3b3c3e',
         borderRadius: 16,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        paddingVertical: hp(0.7),
     },
 });
 
